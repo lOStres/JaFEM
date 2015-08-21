e0eaf3da3d99da72787290d32fdc1476c5ae4b4c
Gryllos Prokopis <gryllosprokopis@gmail.com>
2015-08-21 21:29:21 +0300

    Add content based query

--- testdatab.py
+++ testdatab.py
@@ -1,10 +1,32 @@
 
 import sys
 import psycopg2
-import numpy
 
 from rtree import index
 from readdata import *
+
+# perform k-NN search for file in spatial index
+def contentQuery(directory, filename, k):
+    # first extract features from query file
+    featureVector = extractFeatures(directory,filename)
+
+    # then open and query spatial index
+    p = index.Property()
+    p.dat_extension = 'data'
+    p.idx_extension = 'index'
+    p.dimension = 13
+
+    rtree = index.Index('rtreez', properties=p, interleaved=True)
+    similar = list(rtree.nearest((featureVector[0], featureVector[1],
+        featureVector[2], featureVector[3], featureVector[4],featureVector[5],
+        featureVector[6], featureVector[7], featureVector[8], featureVector[9],
+        featureVector[10], featureVector[11], featureVector[12],
+        featureVector[0], featureVector[1], featureVector[2], featureVector[3],
+        featureVector[4], featureVector[5], featureVector[6], featureVector[7],
+        featureVector[8], featureVector[9], featureVector[10],
+        featureVector[11], featureVector[12]), 5))
+    print(similar)
+
 
 def main():
     # initialize table metadata
@@ -112,6 +134,12 @@ def main():
             if con:
                 con.commit()
                 con.close()
+
+    # content-based query
+    elif len(sys.argv) == 2 and sys.argv[1] == '-q':
+
+        contentQuery("/home/klwnos/Documents/children_playing", "12.mp3", 5)
+
 
 if __name__ == "__main__":
     main()
